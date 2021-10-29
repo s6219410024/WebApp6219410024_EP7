b33577307e787a90d80e5df4cf3fcaac230bb60a
s621941004 <s6219410024@sau.ac.thgit config --global user.email>
2021-10-29 17:22:18 +0700

    Code V.3 : Do zzzzz....

---
+++ js/script9.js
@@ -0,0 +1,38 @@
+console.log('Hi js 9....')
+
+function showName(name){
+    console.log('สวัสดี' + name);
+}
+showName('แมว')
+showName('มด')
+
+function calSum(a, b){
+    console.log('a + b ได้ค่าเป็น ')
+    return a + b;
+}
+console.log(calSum(10, 20))
+//alert( calSum(100, 99))
+
+//let test1 = function(){
+//    console.log('Hello....')
+//}
+
+let test1 = () =>{
+    console.log('Hello....')
+}
+
+//function test1(){
+//    console.log('Hello....')
+//}
+
+let test2 = function(){
+    console.log('Hi....')
+}
+
+let test2 = (number) =>{
+    console.log('Hi....' + number)
+}
+
+test1()
+test1()
+test2()
