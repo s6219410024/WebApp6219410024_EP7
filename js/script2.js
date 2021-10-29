6aeb37b0d751e52489a2753496030da64fc85350
s621941004 <s6219410024@sau.ac.thgit config --global user.email>
2021-10-29 13:32:23 +0700

    Code V.1 : Do xxx

---
+++ js/script2.js
@@ -0,0 +1,27 @@
+console.log("aaabbb")
+console.log("cccddd")
+console.log(5500)
+console.log(85.221)
+console.log("aaa'bbb")
+//console.log('ccc'bbb')
+console.log('ccc\'ddd')
+//console.log("aaa"bbb")
+console.log("aaa\"bbb")
+console.log('aaa"bbb')
+console.log(true)
+console.log(false)
+console.log( 5 + 6 )
+console.log( 5 - 6 )
+console.log( 5 * 6 )
+
+console.log('5' + '6')
+console.log('5' - '6')
+console.log('5' * '6')
+console.log("5" * "6")
+
+console.log("zzz" * "xxx")
+console.log("zzz" + "xxx")
+
+console.log( 8 ** 3)
+console.log( 9 % 4)
+
---
+++ js/script5.js
@@ -0,0 +1,30 @@
+//ตัวแปร Variable
+var a = 20;
+let x = 1000000;//
+
+{
+    let b = 15;
+    var c = 99;
+    const d = 88888;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+    console.log(x);
+    console.log('--------------------------');
+
+    b = 30;
+    a = 100;
+    c = 555
+    //d = 111111;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log('--------------------------');
+}
+
+console.log(a);
+//console.log(b);
+console.log(c);
+//console.log(d);
+console.log('--------------------------');
